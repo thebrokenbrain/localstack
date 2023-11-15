45e2402daceb07a77538dbd1362d6adc94e1e112
Josemi <josemi.rodriguez@outlook.es>
2023-11-15 22:40:56 +0100

    Se para el script en cada operación

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ if __name__ == '__main__':
     print('Creating buckets')
     s3.create_bucket('bucket-1')
     s3.create_bucket('bucket-2')
+    input("Press enter to continue...")
 
     # Se suben objetos al bucket-1.
     print('\nInitializing bucket-1')
@@ -29,12 +30,14 @@ if __name__ == '__main__':
     s3.initialize_bucket('bucket-2', 
                         ['6.jpg', '7.jpg', '8.jpg', '9.jpg'])
     s3.list_bucket_objects('bucket-2')
+    input("Press enter to continue...")
 
     # Se copian los objetos del bucket-1 al bucket-2.
     print('\nCopying objects from bucket-1 to bucket-2')
     s3.copy_objects('bucket-1', 'bucket-2')
     print('\nObjects in bucket-2 after copy objects from bucket-1')
     s3.list_bucket_objects('bucket-2')
+    input("Press enter to continue...")
 
     # Se eliminan los buckets.
     print ('\nDeleting buckets')
